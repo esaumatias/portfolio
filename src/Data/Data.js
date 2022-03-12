e9f9dbfca7507cc4b536322f339b630044de72b4
Esau Matias <esaufreirematias@gmail.com>
2022-03-12 13:11:31 -0300

    fix: (portifolio) - atualiza dados do portifolio

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -3,53 +3,39 @@ import Imagem2 from '../Images/2.png';
 import Imagem3 from '../Images/3.png';
 import Imagem4 from '../Images/4.png';
 import Imagem5 from '../Images/5.png';
-import Imagem6 from '../Images/6.png';
-import Imagem7 from '../Images/7.png';
 
 const projectsArray = [
   {
     id: 1,
-    name: 'Pokedex',
-    link: 'https://pokedex-woad-beta.vercel.app/',
-    image: Imagem6,
-  },
-  {
-    id: 2,
-    name: 'Game da multiplicação',
-    link: 'https://multiplication-table-two.vercel.app/',
-    image: Imagem7,
-  },
-  {
-    id: 3,
-    name: 'Carrinho de compras',
-    link: 'https://esaumatias.github.io/projetos/shopping-cart/',
+    name: 'My Wallet',
+    link: 'https://mywallet-taupe.vercel.app/',
     image: Imagem1,
   },
   {
+    id: 2,
+    name: 'Pokedex',
+    link: 'https://pokedex-woad-beta.vercel.app/',
+    image: Imagem2,
+  },
+  {
+    id: 3,
+    name: 'Game da multiplicação',
+    link: 'https://multiplication-table-two.vercel.app/',
+    image: Imagem3,
+  },
+  {
     id: 4,
-    name: 'Paleta de Cores',
-    link: 'https://esaumatias.github.io/projetos/pixels-art/',
+    name: 'Carrinho de compras',
+    link: 'https://esaumatias.github.io/projetos/shopping-cart/',
     image: Imagem4,
   },
   {
     id: 5,
-    name: 'Sistema solar',
-    link: 'https://esaumatias.github.io/projetos/solar-system/build/index.html',
-    image: Imagem2,
-  },
-  {
-    id: 6,
-    name: 'Trybewarts',
-    link: 'https://esaumatias.github.io/projetos/trybewarts/',
-    image: Imagem3,
-  },
-  {
-    id: 7,
-    name: 'Lista de tarefas',
-    link: 'https://esaumatias.github.io/projetos/todo-list/',
+    name: 'Paleta de Cores',
+    link: 'https://esaumatias.github.io/projetos/pixels-art/',
     image: Imagem5,
   },
-  
+ 
 ];
 
 export default projectsArray;
